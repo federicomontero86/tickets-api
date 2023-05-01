dcf8f741fc17154ef73400d7821e0679a06ce57f
federico.montero <federico.montero@cencosud.com.uy>
2023-05-01 20:52:04 -0300

    prueba gitlab

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,10 +1,10 @@
-import { TicketsController } from './tickets.controller';
-import { TicketsService } from './tickets.service';
-import { Ticket } from './entities/ticket.entity';
+import { TicketsController } from "./tickets.controller";
+import { TicketsService } from "./tickets.service";
+import { Ticket } from "./entities/ticket.entity";
 
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/auth/auth.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket]), AuthModule],
